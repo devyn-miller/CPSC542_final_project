8a60b8d06f514ee7adb77822ec7504f4214a8fd3
Hayden Fargo <haydenfargo@gmail.com>
2024-04-09 11:33:19 -0700

    adding model architecture

--- src/objects/stack.py
+++ src/objects/stack.py
@@ -1,4 +1,5 @@
 from data import Data
+from objects.architecture.conv_autoencoder import ConvAutoencoder
 
 class Stack:
     '''This class holds the data, model, architecture, and results
@@ -19,7 +20,15 @@ class Stack:
         self.test_dataset = test_dataset
         self.val_dataset = val_dataset
         
-    def create_model(self, hp, model_type='ConvAutoencoder'):
+    def create_model(self, hp, model_type='conv_autoencoder'):
+        if model_type is "conv_autoencoder":
+            self.architecture = ConvAutoencoder()
+            self.model = self.architecture(hp)
+            
+            print("hello")
+        elif model_type is "NoGAN":
+            #call NoGAN.py for model creation
+            print("hello")
         '''Creates a ML model using the given model type and updates self.model
         '''
         if model_type is 'ConvAutoencoder':
