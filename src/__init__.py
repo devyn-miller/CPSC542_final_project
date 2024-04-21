3cf9fface5422d3d07ef0b87cbbe355fe335baac
Devyn Miller <144622580+devyn-miller@users.noreply.github.com>
2024-04-21 19:54:06 +0000

    just the movies

--- src/__init__.py
+++
@@ -1,8 +0,0 @@
-
-import os
-import sys
-sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
-sys.path.append(os.path.abspath('../'))  # Adjust the path accordingly
-
-from objects.augmentation import ImageAugmenter  # Now import your module
