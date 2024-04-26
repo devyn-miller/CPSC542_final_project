bd53e51ebfb01c4ec72f22683f11cbac2b16d58b
Tyler L. (desktop) <69596889+tylew@users.noreply.github.com>
2024-04-26 15:08:45 -0700

    modify frame extraction script

--- src/create_video.py
+++ src/create_video.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 
-def process_video_frames(data_path='data'):
+def process_video_frames(data_path='data', frame_skip=30) -> list[tuple]:
     if not os.path.exists(data_path):
         print(f"Error: The directory {data_path} does not exist.")
         return []
@@ -12,26 +12,33 @@ def process_video_frames(data_path='data'):
 
     for video_file in video_files:
         video_path = os.path.join(data_path, video_file)
-        print(f"Processing {video_file}...")
+        print(f"Processing {video_file}... Storing every {frame_skip} frames")
 
         cap = cv2.VideoCapture(video_path)
-        
+
         if not cap.isOpened():
             print(f"Error: Could not open video {video_file}.")
-            continue
+        else:
+            current_frame = 0
+            frames = []  # Ensure this list is defined to store your frames
 
-        while True:
-            ret, frame = cap.read()
-            
-            if not ret:
-                print(f"Finished processing {video_file}. Exiting ...")
-                break
+            while True:
+                ret, frame = cap.read()
 
-            gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                if not ret:
+                    print(f"{current_frame} total frames")
+                    print(f"Finished processing {video_file}. Exiting ...")
+                    break
 
-            frames.append((frame, gray_frame))
+                # Process only every Nth frame (N=frame_skip)
+                if current_frame % frame_skip == 0:
+                    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    frames.append((rgb_frame, gray_frame))
 
-        cap.release()
+                current_frame += 1
+
+            cap.release()
 
 
     return frames
